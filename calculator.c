81109b34a26a545e748d66eac50e0053bf7381bd
Kurt Justin J. Malubay <justinkurt837@gmail.com>
2024-09-15 19:48:46 +0800

    Redid the update because it didn't work

--- calculator.c
+++ calculator.c
@@ -5,6 +5,10 @@ int main()
 {
     int mode;
     int mode2;
+    // Basic Unit Conversion
+    double ft, meters, in, cm, sqft, sqmt, acres, cft, cmeters, pounds, kg, kp, ce, f;
+    // Architecture Math
+    double length, width, area, base, height, radius, perimeter, a, b, c, circumference, volume;
     const double pi = 3.14159265;
 
     printf("(1) Basic Unit Conversion\n(2) Architecture Math\n(3) Normal Calculator\n\nInput answer here: ");
@@ -30,7 +34,6 @@ int main()
             case 1:
                 printf("\n\nFeet to Meters\n");
                 printf("Enter Feet: ");
-                double ft;
                 scanf("%lf", &ft);
                 ft *= 0.3048;
                 printf("The answer is %lf Meters", ft);
@@ -39,7 +42,6 @@ int main()
             case 2:
                 printf("\n\nMeters to Feet\n");
                 printf("Enter Meters: ");
-                double meters;
                 scanf("%lf", &meters);
                 meters *= 3.281;
                 printf("The answer is %.2f Feet", meters);
@@ -48,7 +50,6 @@ int main()
             case 3:
                 printf("\n\nInches to Centimeters\n");
                 printf("Enter Inches: ");
-                double in;
                 scanf("%lf", &in);
                 in *= 2.54;
                 printf("The answer is %lf Centimeters", in);
@@ -57,7 +58,6 @@ int main()
             case 4:
                 printf("\n\nCentimeter to Inches\n");
                 printf("Enter Centimeters: ");
-                double cm;
                 scanf("%lf", &cm);
                 cm *= 0.3937;
                 printf("The answer is %lf Inches", cm);
@@ -66,6 +66,7 @@ int main()
             default:
                 printf("Error! Incorrect input");
             }
+            break;
 
         case 2:
             printf("\n\nArea\n");
@@ -78,7 +79,6 @@ int main()
             case 1:
                 printf("\n\nSquare Feet to Square Meters\n");
                 printf("Enter Square Feet: ");
-                double sqft;
                 scanf("%lf", &sqft);
                 sqft *= 0.0929;
                 printf("The answer is %lf square meters", sqft);
@@ -87,7 +87,6 @@ int main()
             case 2:
                 printf("\n\nSquare Meters to Square Feet\n");
                 printf("Enter Square Meters: ");
-                double sqmt;
                 scanf("%lf", &sqmt);
                 sqmt *= 10.764;
                 printf("The answer is %lf square feet", sqmt);
@@ -96,7 +95,6 @@ int main()
             case 3:
                 printf("\n\nAcres to Square Meters\n");
                 printf("Enter Acres: ");
-                double acres;
                 scanf("%lf", &acres);
                 acres *= 4046.86;
                 printf("The answer is %lf Square Meters", acres);
@@ -105,20 +103,19 @@ int main()
             case 4:
                 printf("\n\nSquare Meters to Acres\n");
                 printf("Enter Square Meters: ");
-                double sqm;
-                scanf("%lf", &sqm);
-                sqm *= 0.000247;
-                printf("The answer is %lf acres", sqm);
+                scanf("%lf", &sqmt);
+                sqmt *= 0.000247;
+                printf("The answer is %lf acres", sqmt);
                 break;
 
             default:
                 printf("Error! Incorrect input");
             }
+            break;
 
         case 3:
             printf("\n\nVolume\n");
             printf("(1) Cubic Feet to Cubic Meters\n(2) Cubic Meters to Cubic Feet\n\nInput answer here: ");
-            int mode;
             scanf("%d", &mode);
 
             switch (mode)
@@ -126,7 +123,6 @@ int main()
             case 1:
                 printf("\n\nCubic Feet to Cubic Meters\n");
                 printf("Enter Cubic Feet: ");
-                double cft;
                 scanf("%lf", &cft);
                 cft *= 0.0283;
                 printf("The answer is %lf cubic meters", cft);
@@ -135,7 +131,6 @@ int main()
             case 2:
                 printf("\n\nCubic Meters to Cubic Feet\n");
                 printf("Enter Cubic Meters");
-                double cmeters;
                 scanf("%lf", &cmeters);
                 cmeters *= 35.314;
                 printf("The answer is %lf cubic feet", cmeters);
@@ -144,6 +139,7 @@ int main()
             default:
                 printf("Error! Incorrect input");
             }
+            break;
 
         case 4:
             printf("\n\nMass\n");
@@ -155,7 +151,6 @@ int main()
             case 1:
                 printf("\n\nPounds to Kilograms\n");
                 printf("Enter Pounds: ");
-                double pounds;
                 scanf("%lf", &pounds);
                 pounds *= 0.4536;
                 printf("The answer is %lf kg", pounds);
@@ -164,7 +159,6 @@ int main()
             case 2:
                 printf("\n\nKilograms to Pounds\n");
                 printf("Enter Kilograms: ");
-                double kg;
                 scanf("%lf", &kg);
                 kg *= 2.205;
                 printf("The answer is %lf kilograms", kg);
@@ -173,6 +167,7 @@ int main()
             default:
                 printf("Error! Incorrect input");
             }
+            break;
 
         case 5:
             printf("\n\nPressure\n");
@@ -184,7 +179,6 @@ int main()
             case 1:
                 printf("\n\nPounds per Square Inch to Kilopascals\n");
                 printf("Enter Pounds per Square Inch: ");
-                double pounds;
                 scanf("%lf", &pounds);
                 pounds *= 6.894;
                 printf("The answer is %lf kilopascals", pounds);
@@ -193,7 +187,6 @@ int main()
             case 2:
                 printf("\n\nKilopascals to Pounds per Square Inch");
                 printf("Enter Kilopascals: ");
-                double kp;
                 scanf("%lf", &kp);
                 kp *= 0.145;
                 printf("The answer is %lf pounds per square inch", kp);
@@ -202,6 +195,7 @@ int main()
             default:
                 printf("Error! Incorrect input");
             }
+            break;
 
         case 6:
             printf("\n\nTemperature\n");
@@ -213,16 +207,14 @@ int main()
             case 1:
                 printf("\n\nCelcius to Fahrenheit\n");
                 printf("Enter Celcius: ");
-                double c;
-                scanf("%lf", &c);
-                c = c * 9 / 5 + 32;
-                printf("The answer is %lf fahrenheit", c);
+                scanf("%lf", &ce);
+                ce = ce * 9 / 5 + 32;
+                printf("The answer is %lf fahrenheit", ce);
                 break;
 
             case 2:
                 printf("\n\nFahrenheit to Celcius\n");
                 printf("Enter Fahrenheit: ");
-                double f;
                 scanf("%lf", &f);
                 f = 26 * 9 / 5 + 32;
                 printf("The answer is %lf celcius", f);
@@ -231,6 +223,7 @@ int main()
             default:
                 printf("Error! Incorrect input");
             }
+            break;
 
         default:
             printf("Error! Incorrect input");
@@ -247,74 +240,57 @@ int main()
         case 1:
             printf("\n\nGet the Area of a Rectangle\n");
             printf("Enter length: ");
-            double length;
             scanf("%lf", &length);
             printf("Enter width: ");
-            double width;
             scanf("%lf", &width);
-            double area1;
-            area1 = length * width;
-            printf("The area of the rectangle is %lf", area1);
+            area = length * width;
+            printf("The area of the rectangle is %lf", area);
             break;
 
         case 2:
             printf("\n\nGet the Area of a Triangle\n");
             printf("Enter base: ");
-            double base;
             scanf("%lf", &base);
             printf("Enter height: ");
-            double height;
             scanf("%lf", &height);
-            double area2;
-            area2 = base * height * 1 / 2;
-            printf("The area of the triangle is %lf", area2);
+            area = base * height * 1 / 2;
+            printf("The area of the triangle is %lf", area);
             break;
 
         case 3:
             printf("\n\nGet the Area of a Circle\n");
             printf("Enter radius: ");
-            double radius;
             scanf("%lf", &radius);
-            double area3;
-            area3 = pi * (radius * radius);
-            printf("The area of the circle is %lf", area3);
+            area = pi * (radius * radius);
+            printf("The area of the circle is %lf", area);
             break;
 
         case 4:
             printf("\n\nGet the Perimeter of a Rectangle\n");
             printf("Enter length: ");
-            double length1;
-            scanf("%lf", &length1);
+            scanf("%lf", &length);
             printf("Enter width: ");
-            double width1;
-            scanf("%lf", &width1);
-            double perimeter;
-            perimeter = 2 * (length1 + width1);
+            scanf("%lf", &width);
+            perimeter = 2 * (length + width);
             printf("The perimeter of the rectangle is %lf", perimeter);
             break;
 
         case 5:
             printf("\n\nGet the Perimeter of a Triangle\n");
             printf("Enter side a: ");
-            double a;
             scanf("%lf", &a);
             printf("Enter side b: ");
-            double b;
             scanf("%lf", &b);
             printf("Enter side c: ");
-            double c;
             scanf("%lf", &c);
-            double perimeter1;
-            perimeter1 = a + b + c;
-            printf("The perimeter of the triangle is %lf", perimeter1);
+            perimeter = a + b + c;
+            printf("The perimeter of the triangle is %lf", perimeter);
             break;
 
         case 6:
             printf("\n\nGet the Circumference of a Circle\n");
             printf("Enter radius: ");
-            double radius1;
-            scanf("%lf", &radius1);
-            double circumference;
+            scanf("%lf", &radius);
             circumference = 2 * pi * radius;
             printf("The circumference of the circle is %lf", circumference);
             break;
@@ -322,67 +298,52 @@ int main()
         case 7:
             printf("\n\nGet the Volume of a Rectangular Prism\n");
             printf("Enter length: ");
-            double length2;
-            scanf("%lf", &length2);
+            scanf("%lf", &length);
             printf("Enter width: ");
-            double width2;
-            scanf("%lf", &width2);
+            scanf("%lf", &width);
             printf("Enter height: ");
-            double height2;
-            scanf("%lf", &height2);
-            double volume2;
-            volume2 = length2 * width2 * height2;
-            printf("The volume of the rectangular prism is: %lf", volume2);
+            scanf("%lf", &height);
+            volume = length * width * height;
+            printf("The volume of the rectangular prism is: %lf", volume);
             break;
 
         case 8:
             printf("\n\nGet the Volume of a Cylinder\n");
             printf("Enter radius: ");
-            double radius3;
-            scanf("%lf", &radius3);
+            scanf("%lf", &radius);
             printf("Enter height: ");
-            double height3;
-            scanf("%lf", &height3);
-            double volume3;
-            volume3 = pi * (radius3 * radius3) * height3;
-            printf("The volume of the cylinder is %lf", volume3);
+            scanf("%lf", &height);
+            volume = pi * (radius * radius) * height;
+            printf("The volume of the cylinder is %lf", volume);
             break;
 
         case 9:
             printf("\n\nGet the Volume of a Sphere\n");
             printf("Enter radius: ");
-            double radius4;
-            scanf("%lf", &radius4);
-            double volume4;
-            volume4 = (4 / 3) * pi * (radius4 * radius4 * radius4);
-            printf("The volume of the sphere is %lf", volume4);
+            scanf("%lf", &radius);
+            volume = (4 / 3) * pi * (radius * radius * radius);
+            printf("The volume of the sphere is %lf", volume);
             break;
 
         case 10:
             printf("\n\nGet the Volume of a Pyramid\n");
             printf("Enter base: ");
-            double base5;
-            scanf("%lf", &base5);
+            scanf("%lf", &base);
             printf("Enter height: ");
-            double height5;
-            scanf("%lf", &height5);
-            double volume5;
-            volume5 = base5 * height5 * 1 / 3;
-            printf("The volume of the pyramid is %lf", volume5);
+            scanf("%lf", &height);
+            volume = base * height * 1 / 3;
+            printf("The volume of the pyramid is %lf", volume);
             break;
 
         case 11:
             printf("\n\nSolve for Pythagorean Theorem\n");
             printf("Enter a: ");
-            double a6;
-            scanf("%lf", &a6);
+            scanf("%lf", &a);
             printf("Enter b: ");
-            double b6;
-            scanf("%lf", &b6);
-            double c6;
-            c6 = (a6 * a6) + (b6 * b6);
-            c6 = sqrtf(c6);
-            printf("The answer is %lf", c6);
+            scanf("%lf", &b);
+            c = (a * a) + (b * b);
+            c = sqrtf(c);
+            printf("The answer is %lf", c);
             break;
 
         case 12:
